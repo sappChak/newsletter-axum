6598b281dca6106fb166fd370225e1edad2c4a65
Andrii Konotop <konotop401@gmail.com>
2024-10-27 12:02:18 +0100

    refactor: rename AppStateInner to Database and update main function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::routing::{get, post};
+use axum::{Extension, Router};
+use newslatter::app_state::Database;
 use newslatter::configuration::get_configuration;
-use newslatter::{
-    app_state::AppStateInner,
-    routes::{health_check, subscribe},
-};
+use newslatter::routes::{health_check, subscribe};
 use std::sync::Arc;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-pub fn app(state: Arc<AppStateInner>) -> Router {
+pub fn routes(state: Arc<Database>) -> Router {
     Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
-        .with_state(state)
+        .layer(Extension(state))
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -31,9 +27,8 @@ async fn main() {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
 
-    let state = Arc::new(AppStateInner::new(&connection_string).await);
-
-    let app = app(state);
+    let state = Arc::new(Database::new(&connection_string).await?);
+    let app = routes(state);
 
     let listener =
         tokio::net::TcpListener::bind(format!("0.0.0.0:{}", configuration.application_port))
@@ -46,4 +41,6 @@ async fn main() {
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
