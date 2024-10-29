a244c16de39217831f70dd01cf65304b7c46feb0
Andrii Konotop <konotop401@gmail.com>
2024-10-29 09:00:42 +0100

    feat: use tracing::Instrument for tracing sqlx queries

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,7 @@ use axum::{Extension, Form};
 use serde::Deserialize;
 use sqlx::types::chrono::Utc;
 use std::sync::Arc;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -17,6 +18,17 @@ pub async fn subscribe(
     Extension(db): Extension<Arc<Database>>,
     Form(form): Form<FormData>,
 ) -> impl IntoResponse {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+    "Adding a new subscriber.",
+    %request_id,
+    subscriber_email = %form.email,
+    subscriber_name= %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
           INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -28,14 +40,15 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(&db.pool)
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            tracing::info!("Subscriber was saved successfully");
+            tracing::info!("New subscriber details have been saved");
             StatusCode::OK
         }
         Err(e) => {
-            tracing::error!("Error executing SQL query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
