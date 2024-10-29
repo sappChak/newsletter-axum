3209063501ae8f414bab7b19b59f55a20d223f40
Andrii Konotop <konotop401@gmail.com>
2024-10-29 20:38:10 +0100

    feat(subscriptions): add tracing instrumentation and refactor subscription logic

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,54 +2,55 @@ use crate::db::database::Database;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Extension, Form};
-use serde::Deserialize;
 use sqlx::types::chrono::Utc;
 use std::sync::Arc;
-use tracing::Instrument;
-use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(db, form),
+    fields(
+      request_id = %uuid::Uuid::new_v4(),
+      subscriber_email = %form.email,
+      subscriber_name= %form.name
+    )
+)]
 pub async fn subscribe(
     Extension(db): Extension<Arc<Database>>,
     Form(form): Form<FormData>,
 ) -> impl IntoResponse {
-    let request_id = Uuid::new_v4();
+    match insert_subscriber(db, form).await {
+        Ok(_) => {
+            tracing::info!("New subscriber details have been saved");
+            StatusCode::OK
+        }
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
 
-    let request_span = tracing::info_span!(
-    "Adding a new subscriber.",
-    %request_id,
-    subscriber_email = %form.email,
-    subscriber_name= %form.name
-    );
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
+#[tracing::instrument(name = "Saving new subscriber details in the database", skip(db, form))]
+pub async fn insert_subscriber(db: Arc<Database>, form: FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
           INSERT INTO subscriptions (id, email, name, subscribed_at)
           VALUES ($1, $2, $3, $4)
         "#,
-        Uuid::new_v4(),
+        uuid::Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
     .execute(&db.pool)
-    .instrument(query_span)
     .await
-    {
-        Ok(_) => {
-            tracing::info!("New subscriber details have been saved");
-            StatusCode::OK
-        }
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
