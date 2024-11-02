99cc85f7bb11c89288a49f8ffa024b2898df14d5
Andrii Konotop <konotop401@gmail.com>
2024-11-02 10:52:19 +0100

    test(domain): add property-based tests for valid email parsing

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -14,7 +14,7 @@ impl SubscriberEmail {
         if s.validate_email() {
             Ok(Self(s))
         } else {
-            Err(format!("{} is not a valid subscriber emai", s))
+            Err(format!("{} is not a valid subscriber email", s))
         }
     }
 }
@@ -22,23 +22,21 @@ impl SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claim::assert_err;
 
-    #[test]
-    fn empty_string_is_rejected() {
-        let email = "".to_string();
-        assert_err!(SubscriberEmail::parse(email));
+    use fake::{faker::internet::en::SafeEmail, Fake};
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(_g: &mut quickcheck::Gen) -> Self {
+            ValidEmailFixture(SafeEmail().fake())
+        }
     }
 
-    #[test]
-    fn email_missing_at_symbol_is_rejected() {
-        let email = "ursuladomain.com".to_string();
-        assert_err!(SubscriberEmail::parse(email));
-    }
-
-    #[test]
-    fn email_missing_subject_is_rejected() {
-        let email = "@domain.com".to_string();
-        assert_err!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        dbg!(&valid_email.0);
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
