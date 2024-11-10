2575f698e66fdf0e61726c7ae914b3269c9e7c24
Andrii Konotop <konotop401@gmail.com>
2024-11-10 19:59:26 +0100

    feat: display error in debug mode and add new test

--- src/ses_workflow.rs
+++ src/ses_workflow.rs
@@ -9,14 +9,14 @@ use aws_sdk_sesv2::Client;
 use crate::domain::SubscriberEmail;
 
 pub struct SESWorkflow {
-    aws_client: Client,
+    client: Client,
     verified_email: SubscriberEmail, // <-- Sender
 }
 
 impl SESWorkflow {
-    pub fn new(aws_client: Client, verified_email: String) -> Self {
+    pub fn new(client: Client, verified_email: String) -> Self {
         Self {
-            aws_client,
+            client,
             verified_email: SubscriberEmail::parse(verified_email).expect("Invalid sender email"),
         }
     }
@@ -43,7 +43,7 @@ impl SESWorkflow {
             .build();
 
         let res = self
-            .aws_client
+            .client
             .send_email()
             .from_email_address(self.verified_email.as_ref())
             .destination(
@@ -51,19 +51,20 @@ impl SESWorkflow {
                     .to_addresses(recipient.as_ref())
                     .build(),
             )
-            .content(email_content);
+            .content(email_content)
+            .send()
+            .await;
 
-        match res.send().await {
+        match res {
             Ok(output) => {
-                if let Some(message_id) = output.message_id {
-                    tracing::info!("Message sent: {}", message_id);
+                if let Some(_message_id) = output.message_id {
                     Ok(())
                 } else {
                     Err(anyhow!("Message sent, but no message ID was returned"))
                 }
             }
             Err(e) => Err(anyhow!(
-                "Error sending welcome email to {}: {}",
+                "Error sending welcome email to {}: {:?}",
                 recipient.as_ref(),
                 e
             )),
@@ -74,7 +75,11 @@ impl SESWorkflow {
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
-    use aws_sdk_sesv2::{operation::send_email::SendEmailOutput, Client};
+    use aws_sdk_sesv2::{
+        operation::send_email::{SendEmailError, SendEmailOutput},
+        types::error::MailFromDomainNotVerifiedException,
+        Client,
+    };
     use aws_smithy_mocks_experimental::{mock, mock_client, RuleMode};
 
     use crate::{domain::SubscriberEmail, ses_workflow::SESWorkflow};
@@ -110,6 +115,45 @@ mod tests {
             .await;
 
         assert!(result.is_ok());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_send_email_fails() -> Result<()> {
+        let mock_send_email = mock!(Client::send_email)
+            .match_requests(|req| {
+                req.destination()
+                    .unwrap()
+                    .to_addresses()
+                    .contains(&"recipient@example.com".into())
+            })
+            .then_error(|| {
+                SendEmailError::MailFromDomainNotVerifiedException(
+                    MailFromDomainNotVerifiedException::builder().build(),
+                )
+            });
+
+        let client = mock_client!(aws_sdk_sesv2, RuleMode::Sequential, [&mock_send_email]);
+
+        let ses_workflow = SESWorkflow::new(client, "sender@example.com".to_string());
+
+        let recipient = SubscriberEmail::parse("recipient@example.com".to_string()).unwrap();
+
+        let result = ses_workflow
+            .send_email(
+                recipient,
+                "Test Subject",
+                "<p>Test HTML content</p>",
+                "Test text content",
+            )
+            .await;
+
+        // Check that the error is propagated
+        assert!(result.is_err());
+        assert!(
+            format!("{result:?}").contains("Error sending welcome email to recipient@example.com:")
+        );
+
         Ok(())
     }
 }
