8997a5f6821c66322e688ec4f8e38b0bc33c99e9
Andrii Konotop <konotop401@gmail.com>
2024-12-16 09:53:28 +0100

    test(api): add test for subscription confirmation link

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -2,6 +2,7 @@ use axum::{
     body::Body,
     http::{Request, StatusCode},
 };
+use reqwest::Url;
 use sqlx::PgPool;
 use tower::util::ServiceExt;
 
@@ -28,4 +29,36 @@ async fn confirmations_without_token_are_rejected_with_a_400(pool: PgPool) {
 
     // Assert
     assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+}
+
+#[sqlx::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called(pool: PgPool) {
+    let app = spawn_test_app(pool).await.unwrap();
+
+    let form_data = "name=Andrii%20Konotop&email=aws.test.receiver@gmail.com";
+
+    let _response = app
+        .router
+        .clone()
+        .oneshot(
+            Request::builder()
+                .method("POST")
+                .uri("/subscriptions")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(Body::from(form_data))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let raw_confirmation_link = &app.get_confirmation_link();
+
+    let confiramation_link = Url::parse(raw_confirmation_link).unwrap();
+
+    assert_eq!(confiramation_link.host_str(), Some("localhost"));
+    assert_eq!(confiramation_link.path(), "/subscriptions/confirm");
+
+    // let response = reqwest::get(confiramation_link).await.unwrap();
+    //
+    // assert_eq!(response.status().as_u16(), 200);
 }
