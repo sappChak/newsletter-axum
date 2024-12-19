a679e6c1156f9ab29f2afdd30e8d1fcb4c156caf
Andrii Konotop <konotop401@gmail.com>
2024-12-19 14:23:54 +0100

    feat: implement subscription confirmation endpoint

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,12 +1,81 @@
+use std::sync::Arc;
+
 use axum::extract::Query;
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Extension;
+use sqlx::PgPool;
+
+use crate::database::db::Database;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     subscription_token: String,
 }
 
-#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters))]
-pub async fn confirm(parameters: Query<Parameters>) -> impl IntoResponse {
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, db))]
+pub async fn confirm(
+    Extension(db): Extension<Arc<Database>>,
+    Query(parameters): Query<Parameters>,
+) -> impl IntoResponse {
+    let subscriber_id =
+        match get_subscriber_id_from_token(&db.pool, &parameters.subscription_token).await {
+            Ok(subscriber_id) => subscriber_id,
+            Err(_) => return StatusCode::BAD_REQUEST,
+        };
+
+    match subscriber_id {
+        Some(subscriber_id) => {
+            if confirm_subscriber(&db.pool, subscriber_id).await.is_ok() {
+                StatusCode::OK
+            } else {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+        }
+        None => StatusCode::NOT_FOUND,
+    };
+
     StatusCode::OK
+}
+
+#[tracing::instrument(name = "Get subscriber ID from token", skip(pool, subscription_token))]
+pub async fn get_subscriber_id_from_token(
+    pool: &PgPool,
+    subscription_token: &str,
+) -> Result<Option<uuid::Uuid>, sqlx::Error> {
+    let subscriber_id = sqlx::query!(
+        r#"
+          SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1
+        "#,
+        &subscription_token
+    )
+    .fetch_optional(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(subscriber_id.map(|id| id.subscriber_id))
+}
+
+#[tracing::instrument(name = "Update subscription status", skip(pool))]
+pub async fn confirm_subscriber(
+    pool: &PgPool,
+    subscriber_id: uuid::Uuid,
+) -> Result<(), sqlx::Error> {
+    let _ = sqlx::query!(
+        r#"
+      UPDATE subscriptions SET status = 'confirmed' WHERE id = $1
+    "#,
+        subscriber_id
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    });
+
+    Ok(())
 }
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -51,7 +51,6 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called(pool: PgPool) {
 
     let response = app
         .router
-        .clone()
         .oneshot(
             Request::builder()
                 .method("GET")
@@ -63,4 +62,50 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called(pool: PgPool) {
         .unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+#[sqlx::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber(pool: PgPool) {
+    // Arrange
+    let app = spawn_test_app(pool).await.unwrap();
+    let form_data = "name=Andrii%20Konotop&email=aws.test.receiver@gmail.com";
+
+    // Act
+    let _ = app
+        .router
+        .clone()
+        .oneshot(
+            Request::builder()
+                .method("POST")
+                .uri("/subscriptions")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(Body::from(form_data))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let confirmation_link = &app.get_confirmation_links().plain_text;
+
+    let _response = app
+        .router
+        .oneshot(
+            Request::builder()
+                .method("GET")
+                .uri(confirmation_link.as_str())
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
+        .fetch_one(&app.db.pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    // Assert
+    assert_eq!(saved.name, "Andrii Konotop");
+    assert_eq!(saved.email, "aws.test.receiver@gmail.com");
+    assert_eq!(saved.status, "confirmed");
 }
